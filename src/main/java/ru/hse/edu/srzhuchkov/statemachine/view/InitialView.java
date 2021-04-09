d4b8c0ce93ef7bc52265e892dfad67598067dad1
Svyatoslav Zhuchkov <sv.zhuchkov@mail.ru>
2021-04-09 21:56:05 +0300

    Добавление клавиатуры перенесено в базовый класс

--- src/main/java/ru/hse/edu/srzhuchkov/statemachine/view/InitialView.java
+++ src/main/java/ru/hse/edu/srzhuchkov/statemachine/view/InitialView.java
@@ -1,30 +1,8 @@
 package ru.hse.edu.srzhuchkov.statemachine.view;
 
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import ru.hse.edu.srzhuchkov.statemachine.State;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class InitialView extends StateView {
-    /**
-     * Sets the keyboard view for the state
-     *
-     * @return the keyboard
-     */
-    @Override
-    public ReplyKeyboard display() {
-        List<KeyboardRow> keyboard = new ArrayList<>();
-        for (String str : getState().getReplies()) {
-            KeyboardRow row = new KeyboardRow();
-            row.add(str);
-            keyboard.add(row);
-        }
-        return new ReplyKeyboardMarkup(keyboard, true, true, true);
-    }
-
     /**
      * Returns the associated state
      *
